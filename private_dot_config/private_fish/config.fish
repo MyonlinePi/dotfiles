a6b08223f480cfc104d134332cb712251086744c
MyonlinePi <myonlinepi@ab10.uk>
2025-07-26 16:13:49 +0100

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -35,7 +35,7 @@ abbr --add ltt eza -algT -s=modified --icons --group-directories-first
 abbr --add aud "sudo apt update && apt list --upgradeable"
 abbr --add aug "sudo apt full-upgrade -y && sudo apt autoremove"
 abbr --add lzd lazydocker
-abbr --add dcu "docker compose up -d"'
+abbr --add dcu "docker compose up -d"
 
 # replaces !! with the result of last_history_item Similar to !! bash history expansion 
 function last_history_item
