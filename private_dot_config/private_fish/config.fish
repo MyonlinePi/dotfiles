0ba6277b9ab14d205062daad99218ea4b0b0782e
MyonlinePi <myonlinepi@ab10.uk>
2026-01-11 12:22:12 +0000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,6 +5,13 @@ function fish_greeting
 end
 #
 # Commands to run in interactive sessions can go here
+#
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)" 
+eval set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+
 set -Ux LANG en_GB.UTF-8
 set -Ux LANGUAGE en_GB:en
 set -Ux LC_ALL en_GB.UTF-8
@@ -60,3 +67,11 @@ abbr -a !! --position anywhere --function last_history_item
 #
 starship init fish | source
 end
+
+#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)" eval set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+#set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+#set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+#fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+#if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+#if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; end;
+#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
